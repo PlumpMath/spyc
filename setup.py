3e8eed4c604e5cbcd30b37045d6530b8b689c5eb
Ian Denhardt <ian@zenhack.net>
2015-05-24 20:44:11 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='spyc',
+      version='0.1',
+      packages=['spyc'],
+
+      author='Ian Denhardt',
+      author_email='ian@zenhack.net',
+      description='Prototype IT automation system',
+      license='LGPL-2.1',
+      url='https://github.com/zenhack/spyc',
+      )
